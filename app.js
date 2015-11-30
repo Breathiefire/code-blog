2088f0e42b9a734c022fe6acac39f99ce878526f
Kay Ouchida <k.e.ouchida@gmail.com>
2015-11-30 15:22:44 -0800

    Got blog.rawData to print to console

---
+++ app.js
@@ -0,0 +1,10 @@
+var blog = {};
+
+function articleTemp(obj) {
+  this.title = obj.title;
+  this.category = obj.category;
+  this.author = obj.author;
+  this.authorUrl = obj.authorUrl;
+  this.publishedOn = obj.publishedOn;
+  this.body = obj.body;
+}
