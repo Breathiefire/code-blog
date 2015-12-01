b69068f3207f702ed4a2fafc574ca5388fa1df03
Kay Ouchida <k.e.ouchida@gmail.com>
2015-11-30 19:29:34 -0800

    Created function for sorting articles by most recent date

--- app.js
+++ app.js
@@ -5,25 +5,40 @@ function makeArticle(obj) {
   this.authorUrl = obj.authorUrl;
   this.publishedOn = obj.publishedOn;
   this.body = obj.body;
+  var date = new Date(this.publishedOn);
+  var today = new Date();
+  this.daysElapsed = Math.round((today - date) / 1000 / 60 / 60 / 24);
 }
 
 makeArticle.prototype.toHtml = function() {
   var $newArticle = $('article.arTemplate').clone();
   $newArticle.removeClass('arTemplate');
   $newArticle.find('h1').html(this.title);
-  $newArticle.find('.byline').html(this.category);
-  $newArticle.find('.byline').html(this.author);
-  $newArticle.find('.byline').html(this.authorURL);
-  $newArticle.find('.byline').html(this.publishedOn);
+  $newArticle.find('.categoryline').html(this.category);
+  $newArticle.find('.authorLine').html(this.author);
+  $newArticle.find('.authorURLline').html(this.authorURL);
+  $newArticle.find('.publishedOnLine').html(this.publishedOn);
+  $newArticle.find('time').html('exactly ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.find('.article-body').html(this.body);
   $newArticle.append('<hr>');
   return $newArticle;
 }
 
 $(function() {
-  for (var i = 0; i <= 21; i++) {
-    var arObj0 = new makeArticle(blog.rawData[i]);
+  var dataSpace = [];
+    for (var i = 0; i < blog.rawData.length; i++) {
+      dataSpace = new makeArticle(blog.rawData[i]);
+      $('#articles').append(dataSpace.toHtml());
+        console.log(dataSpace);
   }
+});
 
-  $('#articles').append(arObj0.toHtml());
+
+blog.rawData.sort(function(one, two){
+  var dateOne=one.publishedOn, dateTwo=two.publishedOn;
+  if (dateOne > dateTwo)
+    return -1;
+  if (dateOne < dateTwo)
+    return 1;
+  return 0;
 });
