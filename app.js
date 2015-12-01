cad70acb98d2f799f008cb8f18a605a877240753
Kay Ouchida <k.e.ouchida@gmail.com>
2015-11-30 16:59:06 -0800

    created method to get each part of article

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var blog = {};
-
-function articleTemp(obj) {
+function makeArticle(obj) {
   this.title = obj.title;
   this.category = obj.category;
   this.author = obj.author;
@@ -8,3 +6,24 @@ function articleTemp(obj) {
   this.publishedOn = obj.publishedOn;
   this.body = obj.body;
 }
+
+makeArticle.prototype.toHtml = function() {
+  var $newArticle = $('article.arTemplate').clone();
+  $newArticle.removeClass('arTemplate');
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('.byline').html(this.category);
+  $newArticle.find('.byline').html(this.author);
+  $newArticle.find('.byline').html(this.authorURL);
+  $newArticle.find('.byline').html(this.publishedOn);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.append('<hr>');
+  return $newArticle;
+}
+
+$(function() {
+  for (var i = 0; i <= 21; i++) {
+    var arObj0 = new makeArticle(blog.rawData[i]);
+  }
+
+  $('#articles').append(arObj0.toHtml());
+});
